5dc35b5a437277b61d994f52758d81ea1f3500ed
nprimo <primo.niccolo@gmail.com>
2022-02-04 11:48:57 +0000

    Add check_map.c
    Update so_long.h accordingly with prototypes

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 16:14:59 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/03 18:09:28 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/02/04 11:48:00 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <fcntl.h>
 
 # define U 50
-# define BUFF_SIZE 4 
+# define BUFF_SIZE 4096 
 
 typedef struct s_point {
 	int	x;
@@ -43,5 +43,10 @@ typedef struct s_data {
 void	ft_mlx_pixel_put(t_data *data, int x, int y, int color);
 int		close_app(int keycode, t_vars *vars);
 char	**get_map(int fd);
+
+// Check maps functions
+int	is_rect(char **map);
+int	is_closed(char **map);
+int	is_valid_map(char **map);
 
 #endif
---
+++ src/check_map.c
@@ -0,0 +1,136 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_map.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/03 18:14:54 by nprimo            #+#    #+#             */
+/*   Updated: 2022/02/04 11:48:14 by nprimo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+typedef struct s_checker
+{
+	int	clt;
+	int	ext;
+	int	str;
+}	t_checker;
+
+static int	is_valid_char(char c)
+{
+	if (c == '0' || c == '1' || c == 'C' || c == 'P' || c == 'E')
+		return (1);
+	else
+		return (0);
+}
+
+static int	*get_dim(char **map)
+{
+	int	n_rows;
+	int	*dim;
+
+	dim = malloc(sizeof(*dim) * 2);
+	if (!dim)
+		return (0);
+	n_rows = 0;
+	dim[1] = ft_strlen(*map);
+	while (*map)
+	{
+		n_rows++;
+		map++;
+	}
+	dim[0] = n_rows++;
+	return (dim);
+}
+
+int	is_rect(char **map)
+{
+	size_t	len;
+
+	len = ft_strlen(*map);
+	map++;
+	while (*map)
+	{
+		if (len != ft_strlen(*map))
+			return (0);
+		map++;
+	}
+	return (1);
+}
+
+int	is_closed(char **map)
+{
+	int	*dim;
+	int	row;
+	int	col;
+
+	dim = get_dim(map);
+	if (!dim)
+		return (0);
+	row = 0;
+	while (row < dim[0])
+	{
+		col = 0;
+		while (col < dim[1])
+		{
+			if ((row == 0 || row == dim[0] - 1) && map[row][col] != '1')
+				return (0);
+			if ((col == 0 || (col == dim[1] - 1)) && map[row][col] != '1')
+				return (0);
+			col++;
+		}
+		row++;
+	}
+	return (1);
+}
+
+int	is_valid_map(char **map)
+{
+	t_checker	check;
+
+	check.clt = 0;
+	check.ext = 0;
+	check.str = 0;
+	while (*map)
+	{
+		while (**map)
+		{
+			if (!is_valid_char(**map))
+				return (0);
+			if (**map == 'C')
+				check.clt++;
+			else if (**map == 'E')
+				check.ext++;
+			else if (**map == 'P')
+				check.str++;
+			(*map)++;
+		}
+		map++;
+	}
+	if (check.clt == 0 || check.ext == 0 || check.str != 1)
+		return (0);
+	return (1);
+}
+
+/*
+int	main(int ac, char **av)
+{
+	int		fd;
+	char	**map;
+
+	if (ac == 2)
+	{
+		fd = open(av[1], O_RDONLY);
+		map = get_map(fd);
+		if (!map)
+			return (1);
+		is_rect(map);
+		is_closed(map);
+		is_valid_map(map);
+	}
+	return (0);
+}
+*/

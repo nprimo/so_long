306fe5db01ee852c7e2253f2b8bd3a60982c9986
nprimo <primo.niccolo@gmail.com>
2022-02-14 11:08:15 +0000

    Add free_vars.c

---
+++ src/free_vars.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_vars.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/14 11:06:12 by nprimo            #+#    #+#             */
+/*   Updated: 2022/02/14 11:07:14 by nprimo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	free_vars(t_vars *v)
+{
+	int	count;
+
+	free(v->dim);
+	free_split(v->map);
+	count = 1;
+	while (count <= 5)
+	{
+		mlx_destroy_image(v->mlx, v->tiles.img[count]);
+		count++;
+	}
+	mlx_destroy_window(v->mlx, v->win);
+	exit(0);
+}

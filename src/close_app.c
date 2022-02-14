e071b5db97f0e907e546f1fea3d97775b7d621d8
nprimo <primo.niccolo@gmail.com>
2022-02-14 14:28:16 +0000

    Add close_app.c

--- src/close_app.c
+++ src/close_app.c
@@ -1,29 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   close_app.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/11 17:38:58 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/14 14:27:48 by nprimo           ###   ########.fr       */
+/*   Created: 2022/02/14 11:17:00 by nprimo            #+#    #+#             */
+/*   Updated: 2022/02/14 12:50:25 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	render_map(t_vars *v);
-int	close_app(int keycode , t_vars *v);
-
-int	main(int ac, char **av)
+int	close_app(int keycode , t_vars *v)
 {
-	t_vars	v;
-
-	if (ac == 2)
+	if (keycode == 53)
 	{
-		if (!init_session(av[1], &v))
-			return (0);
-		mlx_hoox(v.mlx, 17, 0, close_app, &v);
-		mlx_loop(v.mlx);
+		free_vars(v);
+		exit(0);
 	}
+	return (1);
 }

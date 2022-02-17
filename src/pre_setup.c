9d4154ced2065d6cfacf598fd4265a7e3ed917f1
nprimo <primo.niccolo@gmail.com>
2022-02-17 16:30:20 +0000

    Update pre_setup.c

--- src/pre_setup.c
+++ src/pre_setup.c
@@ -6,19 +6,22 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 11:47:31 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/17 12:08:11 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/02/17 15:41:39 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	pre_setup(t_game *game)
+t_game	pre_setup(void)
 {
-	game->mlx = NULL;
-	game->win = NULL;
-	game->map = NULL;
-	game->tiles.img[0] = NULL;
-	game->tiles.img[1] = NULL;
-	game->tiles.img[2] = NULL;
-	game->tiles.img[3] = NULL;
+	t_game	game;
+
+	game.mlx = NULL;
+	game.win = NULL;
+	game.map = NULL;
+	game.tiles.img[0] = NULL;
+	game.tiles.img[1] = NULL;
+	game.tiles.img[2] = NULL;
+	game.tiles.img[3] = NULL;
+	return (game);
 }

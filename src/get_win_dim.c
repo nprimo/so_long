d817a3d5047d27bc6a344504667637a31e08ddc2
nprimo <primo.niccolo@gmail.com>
2022-02-17 12:36:37 +0000

    Rename src/get_dim.c into get_win_dim.c
    Update get_dim old function with 2 separate functions returning
    win_row and win_col separately

--- src/get_win_dim.c
+++ src/get_win_dim.c
@@ -1,32 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_dim.c                                          :+:      :+:    :+:   */
+/*   get_win_dim.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/11 16:09:02 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/11 16:09:19 by nprimo           ###   ########.fr       */
+/*   Created: 2022/02/17 11:34:31 by nprimo            #+#    #+#             */
+/*   Updated: 2022/02/17 12:23:54 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	*get_dim(char **map)
+int	get_win_col(char **map)
+{
+	return (ft_strlen(*map));
+}
+
+int	get_win_row(char **map)
 {
 	int	n_rows;
-	int	*dim;
 
-	dim = malloc(sizeof(*dim) * 2);
-	if (!dim)
-		return (NULL);
 	n_rows = 0;
-	dim[1] = ft_strlen(*map);
 	while (*map)
 	{
 		n_rows++;
 		map++;
 	}
-	dim[0] = n_rows++;
-	return (dim);
+	return (n_rows);
 }

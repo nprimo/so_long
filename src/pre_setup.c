994d824b27241d3a6da94f902b8953da172fb1f0
nprimo <primo.niccolo@gmail.com>
2022-02-17 12:13:26 +0000

    Update pre_setup.c
    Add initialization for tiles.img[n]

--- src/pre_setup.c
+++ src/pre_setup.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 11:47:31 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/17 11:53:46 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/02/17 12:08:11 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,8 @@ void	pre_setup(t_game *game)
 	game->mlx = NULL;
 	game->win = NULL;
 	game->map = NULL;
+	game->tiles.img[0] = NULL;
+	game->tiles.img[1] = NULL;
+	game->tiles.img[2] = NULL;
+	game->tiles.img[3] = NULL;
 }

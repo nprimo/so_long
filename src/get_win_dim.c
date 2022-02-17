3ce16a713d8588b96d85d77b80ac206c421be9d7
nprimo <primo.niccolo@gmail.com>
2022-02-17 18:06:37 +0000

    Update get_win_dim.c
    Update type of function inputs

--- src/get_win_dim.c
+++ src/get_win_dim.c
@@ -6,18 +6,25 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 11:34:31 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/17 17:12:34 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/02/17 18:00:36 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	get_win_col(char **map)
+int	get_win_col(t_tile_type **map)
 {
-	return (ft_strlen(*map));
+	t_tile_type	*map_row;
+	int			col;
+
+	col = 0;
+	map_row = *map;
+	while (map_row[col] != END_ROW)
+		col++;
+	return (col);
 }
 
-int	get_win_row(char **map)
+int	get_win_row(void **map)
 {
 	int	n_rows;
 

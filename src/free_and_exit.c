d206e9d152feec1b28a882f04886876d298b9026
nprimo <primo.niccolo@gmail.com>
2022-02-17 16:46:03 +0000

    Update free_and_exit.c

--- src/free_and_exit.c
+++ src/free_and_exit.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 11:56:03 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/17 12:20:38 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/02/17 16:45:44 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ int	free_and_exit(t_game *game)
 {
 	int	count;
 
-	count = 4;
-	while (count > 0)
+	count = 0;
+	while (count < 4)
 	{
 		if (game->tiles.img[count])
 			mlx_destroy_image(game->mlx, game->tiles.img[count]);
-		count--;
+		count++;
 	}
 	if (game->win)
 		mlx_destroy_window(game->mlx, game->win);

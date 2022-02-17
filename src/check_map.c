208de7606eebbd11e46beeb99c0120b2e22b271b
nprimo <primo.niccolo@gmail.com>
2022-02-17 12:33:04 +0000

    Update check_map.c

--- src/check_map.c
+++ src/check_map.c
@@ -6,42 +6,42 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 13:44:42 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/17 11:23:04 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/02/17 12:32:56 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static int	is_closed(char **map, int row, int col)
-{
-	int		*dim;
+static int	is_closed(char **map);
 
-	dim = get_dim(map);
-	if (!dim)
+int	check_map(char **map)
+{
+	if (!is_closed(map))
 		return (0);
+	return (1);
+}
+
+static int	is_closed(char **map)
+{
+	int		win_row;
+	int		win_col;
+	int		row;
+	int		col;
+
+	win_row = get_win_row(map);
+	win_col = get_win_col(map);
 	row = 0;
-	while (row < dim[0])
+	while (row < win_row)
 	{
 		col = 0;
-		while (col < dim[1])
+		while (col < win_col)
 		{
-			if ((row == 0 || row == dim[0] - 1
-					|| col == 0 || col == dim[1] - 1) && map[row][col] != '1')
-			{
-				free(dim);
+			if ((row == 0 || row == win_row - 1
+					|| col == 0 || col == win_col - 1) && map[row][col] != '1')
 				return (0);
-			}
 			col++;
 		}
 		row++;
 	}
-	free(dim);
-	return (1);
-}
-
-int	check_map(char **map)
-{
-	if (!is_closed(map, 0, 0))
-		return (0);
 	return (1);
 }

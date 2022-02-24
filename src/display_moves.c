cd56900b12b8586f18afb482b1a2b079022552f0
nprimo <primo.niccolo@gmail.com>
2022-02-24 11:47:03 +0000

    Update display_moves.c

--- src/display_moves.c
+++ src/display_moves.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 10:36:24 by nprimo            #+#    #+#             */
-/*   Updated: 2022/02/24 10:42:36 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/02/24 11:45:21 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,18 @@
 
 void	display_moves(t_game *game)
 {
+	int	col;
+
+	col = 0;
+	while (col < 2)
+	{
+		mlx_put_image_to_window(game->mlx, game->win,
+			game->tiles.img[T_BACKGROUND], col * U, 0);
+		mlx_put_image_to_window(game->mlx, game->win,
+			game->tiles.img[T_WALL], col * U, 0);
+		col++;
+	}
 	mlx_string_put(game->mlx, game->win,
-		0, 0, 0x00000000, ft_itoa(game->player.moves));
+		U / 4, U / 4, 0x00000000, ft_itoa(game->player.moves));
+	ft_printf("Moves: %d\n", game->player.moves);
 }
